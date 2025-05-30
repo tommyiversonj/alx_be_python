e2af3a068ab936918ddbee7cd0060cefddb3b7bf
tommyiversonj <tommyiversonj@gmail.com>
2025-05-29 21:36:37 -0700

    Fixed formular issue

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,9 +3,9 @@
 monthly_income = float(input("Enter your monthly income: "))
 monthly_expense = float(input("Enter your total monthly expenses: "))
 
-monthly_saving = monthly_income - monthly_expense
+monthly_savings = monthly_income - monthly_expense
 
-project_savings = monthly_income * 12 + (monthly_income * 12 * 0.05)
+project_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
-print(f"Your monthly savings are ${monthly_saving:.2f}.")
+print(f"Your monthly savings are ${monthly_savings:.2f}.")
 print(f"Projected savings after one year, with interest, is: ${project_savings:.2f}.")
