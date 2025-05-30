07cb076556acd07f7a90f085b50c10691109e7b6
tommyiversonj <tommyiversonj@gmail.com>
2025-05-29 18:46:35 -0700

    Update with Convert Hours to Seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,10 @@
+# Demonstrate understanding of variable assignments and arithmetic operations by converting a given number of hours into seconds.
+
+# Define value
+hours = 2
+
+# Convert hours to second
+seconds = hours * 3600
+
+#Display result
+print(f"{hours} hour(s) is {seconds} seconds")
